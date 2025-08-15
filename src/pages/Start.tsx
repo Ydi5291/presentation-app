2399c52ee9ca50a437b771b9878c54ff2e955793
DOC.WOW Restaurant <contact@docwow.de>
2025-08-15 16:05:40 +0200

    footer et autres ajustement

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -4,27 +4,27 @@ import './Start.css';
 const Start = () => {
   return (
     <>
-    <div className="start-intro">
-      <h2>Willkommen auf meinem Portfolio</h2>
-      <div className="start-text">
-        <p>
-          Hier finden Sie eine Auswahl meiner Arbeiten und Projekte.
-        </p>
+      <div className="start-intro">
+        <h2>Willkommen auf meinem Portfolio</h2>
+        <div className="start-text">
+          <p>
+            Hier finden Sie eine Auswahl meiner Arbeiten und Projekte.
+          </p>
+        </div>
       </div>
-    </div>
 
-    <div className="start-container">
-      <p className="start-text">
-        Ich bin ein leidenschaftlicher Frontend-Entwickler mit Fokus auf moderne Webtechnologien wie React und Firebase.<br />
-        Erfahrung in der Entwicklung benutzerfreundlicher Webanwendungen, API-Integration und Automatisierung von KI-Chatboten mit Make.com und n8n.<br />
-        Hohe Lernbereitschaft und praxisorientierte Projekte.
-      </p>
-      <img
-        src={devImg}
-        alt="Développeur"
-        className="start-photo"
-      />
-    </div>
+      <div className="start-container">
+        <p className="start-text">
+          Ich bin ein leidenschaftlicher Frontend-Entwickler mit Fokus auf moderne Webtechnologien wie React und Firebase.<br />
+          Erfahrung in der Entwicklung benutzerfreundlicher Webanwendungen, API-Integration und Automatisierung von KI-Chatboten mit Make.com und n8n.<br />
+          Hohe Lernbereitschaft und praxisorientierte Projekte.
+        </p>
+        <img
+          src={devImg}
+          alt="Développeur"
+          className="start-photo"
+        />
+      </div>
     </>
   );
 };
