50c0dd41935c741ab410d9f77524359f8bb8c9b9
DOC.WOW Restaurant <contact@docwow.de>
2025-10-03 00:04:46 +0200

    style: amélioration des pages About et Start

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,29 +1,27 @@
-import devImg from "../assets/devImg.png";
+import { useNavigate } from "react-router-dom";
 import './Start.css';
+import ydiImg from "../assets/YDi.jpg";
 
 const Start = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="start-intro">
-        <h2>Willkommen auf meinem Portfolio</h2>
-        <div className="start-text">
-          <p>
-            Hier finden Sie eine Auswahl meiner Arbeiten und Projekte.
-          </p>
+      <div className="container-intro">
+        <img src={ydiImg} alt="Youssouf Diallo" className="start-photo" />
+        <div className="intro-text">
+          <h2 className="intro-name">Ich bin Youssouf</h2>
+          <p className="intro-profession">Frontend-Entwickler und UI/UX Designer</p>
         </div>
       </div>
 
       <div className="start-container">
         <p className="start-text">
           Ich bin ein leidenschaftlicher Frontend-Entwickler mit Fokus auf moderne Webtechnologien wie React und Firebase.<br />
-          Erfahrung in der Entwicklung benutzerfreundlicher Webanwendungen, API-Integration und Automatisierung von KI-Chatboten mit Make.com und n8n.<br />
-          Hohe Lernbereitschaft und praxisorientierte Projekte.
+          Erfahrung in der Entwicklung benutzerfreundlicher Webanwendungen, API-Integration und Automatisierung von KI-Chatboten mit Make.com .<br />
+          Hohe Lernbereitschaft und praxisorientierte Projekte. Außerdem arbeite ich sicher mit Content-Management-Systemen wie WordPress.
         </p>
-        <img
-          src={devImg}
-          alt="Développeur"
-          className="start-photo"
-        />
+        <button onClick={() => navigate("/about")}>weiter lesen</button>
       </div>
     </>
   );
